8a6b8573baae9222d947083ed284eacbd61e2b55
Mingyu <mingyu.tang@salesforce.com>
2018-12-04 21:35:23 -0800

    improved LC269

--- src/LC269.java
+++ src/LC269.java
@@ -11,37 +11,29 @@ public class LC269 {
             }
         }
 
-        // build the trie
-        TrieNode trieRoot = new TrieNode('\0');
-        for (String word : words) {
-            trieRoot.addSequence(word);
+        if (words.length > 1) {
+            // find the edges and add to the graph
+            for (int i=0; i<words.length - 1; i++) {
+                addEdges(words[i], words[i+1], g);
+            }
         }
-
-        // traverse the trie to build the graph
-        trieToGraph(trieRoot, g);
 
         // do a topological sort of the graph
         // the result is the order
         return g.topoSort();
     }
 
-    static void trieToGraph(TrieNode t, Graph g) {
-        List<TrieNode> children = t.getChildren();
-        int len = children.size();
-        if (len == 0) {
-            return;
-        }
-
-        if (len > 1) {
-            for (int i = 0; i < len - 1; i++) {
-                TrieNode t1 = children.get(i);
-                TrieNode t2 = children.get(i + 1);
-                g.addEdge(t1.getCh(), t2.getCh());
+    static void addEdges(String w1, String w2, Graph g) {
+        int len1 = w1.length();
+        int len2 = w2.length();
+        int len = len1 <= len2 ? len1 : len2;
+        for (int i = 0; i < len; i++) {
+            char c1 = w1.charAt(i);
+            char c2 = w2.charAt(i);
+            if (c1 != c2) {
+                g.addEdge(c1, c2);
+                break;
             }
-        }
-
-        for (TrieNode child : children) {
-            trieToGraph(child, g);
         }
     }
 
@@ -57,138 +49,103 @@ public class LC269 {
         String order = alienOrder(words);
         System.out.println(order);
     }
-}
 
-class TrieNode {
-    private char ch;
-    private Map<Character, TrieNode> childrenMap;
-    private List<TrieNode> children;
+    static class GraphNode {
+        private char ch;
+        private int state; // 0 - not visited, 1 - visiting, 2 - visited
+        private Map<Character, GraphNode> adjacencyList;
 
-    TrieNode(char ch) {
-        this.ch = ch;
-        this.childrenMap = new HashMap<>();
-        this.children = new ArrayList<>();
-    }
-
-    void addSequence(CharSequence seq) {
-        char c = seq.charAt(0);
-        TrieNode t = childrenMap.get(c);
-        if (t == null) {
-            t = new TrieNode(c);
-            childrenMap.put(c, t);
-            children.add(t);
+        GraphNode(char ch) {
+            this.ch = ch;
+            adjacencyList = new HashMap<>();
+            state = 0;
         }
-        int len = seq.length();
-        if (len > 1) {
-            seq = seq.subSequence(1, len);
-            t.addSequence(seq);
+
+        Map<Character, GraphNode> getAdjacencyList() {
+            return adjacencyList;
         }
-    }
 
-    List<TrieNode> getChildren() {
-        return children;
-    }
+        int getState() {
+            return state;
+        }
 
-    char getCh() {
-        return ch;
-    }
-}
+        void setState(int value) {
+            state = value;
+        }
 
-class GraphNode {
-    private char ch;
-    private int state; // 0 - not visited, 1 - visiting, 2 - visited
-    private Map<Character, GraphNode> adjacencyList;
-
-    GraphNode(char ch) {
-        this.ch = ch;
-        adjacencyList = new HashMap<>();
-        state = 0;
-    }
-
-    Map<Character, GraphNode> getAdjacencyList() {
-        return adjacencyList;
-    }
-
-    int getState() {
-        return state;
-    }
-
-    void setState(int value) {
-        state = value;
-    }
-
-    char getCh() {
-        return ch;
-    }
-}
-
-class Graph {
-    private Map<Character, GraphNode> nodes;
-
-    Graph() {
-        nodes = new HashMap<>();
-    }
-
-    void addNode(char c) {
-        GraphNode node = nodes.get(c);
-        if (node == null) {
-            node = new GraphNode(c);
-            nodes.put(c, node);
+        char getCh() {
+            return ch;
         }
     }
 
-    void addEdge(char from, char to) {
-        GraphNode n1 = nodes.get(from);
-        GraphNode n2 = nodes.get(to);
-        Map<Character, GraphNode> adjList = n1.getAdjacencyList();
-        if (!adjList.containsKey(to)) {
-            adjList.put(to, n2);
-        }
-    }
+    static class Graph {
+        private Map<Character, GraphNode> nodes;
 
-    /**
-     * Topological sort of the graph
-     * @return the ordered characters in a string. empty if the graph is not a DAG.
-     */
-    String topoSort() {
-        StringBuilder sb = new StringBuilder();
-        boolean success = true;
-        for (GraphNode n: nodes.values()) {
-            if (!topoSort(n, sb)) {
-                success = false;
-                break;
+        Graph() {
+            nodes = new HashMap<>();
+        }
+
+        void addNode(char c) {
+            GraphNode node = nodes.get(c);
+            if (node == null) {
+                node = new GraphNode(c);
+                nodes.put(c, node);
             }
         }
 
-        if (!success) {
-            return "";
-        } else {
-            // reverse the string and return
-            return sb.reverse().toString();
-        }
-    }
-
-    static boolean topoSort(GraphNode node, StringBuilder sb) {
-        int state = node.getState();
-        if (state == 1) {
-            // not a DAG
-            return false;
+        void addEdge(char from, char to) {
+            GraphNode n1 = nodes.get(from);
+            GraphNode n2 = nodes.get(to);
+            Map<Character, GraphNode> adjList = n1.getAdjacencyList();
+            if (!adjList.containsKey(to)) {
+                adjList.put(to, n2);
+            }
         }
 
-        if (state == 2) {
-            return true;
+        /**
+         * Topological sort of the graph
+         * @return the ordered characters in a string. empty if the graph is not a DAG.
+         */
+        String topoSort() {
+            StringBuilder sb = new StringBuilder();
+            boolean success = true;
+            for (GraphNode n: nodes.values()) {
+                if (!topoSort(n, sb)) {
+                    success = false;
+                    break;
+                }
+            }
+
+            if (!success) {
+                return "";
+            } else {
+                // reverse the string and return
+                return sb.reverse().toString();
+            }
         }
 
-        node.setState(1);
-
-        Map<Character, GraphNode> adjList = node.getAdjacencyList();
-        for (GraphNode n : adjList.values()) {
-            if (!topoSort(n, sb)) {
+        static boolean topoSort(GraphNode node, StringBuilder sb) {
+            int state = node.getState();
+            if (state == 1) {
+                // not a DAG
                 return false;
             }
+
+            if (state == 2) {
+                return true;
+            }
+
+            node.setState(1);
+
+            Map<Character, GraphNode> adjList = node.getAdjacencyList();
+            for (GraphNode n : adjList.values()) {
+                if (!topoSort(n, sb)) {
+                    return false;
+                }
+            }
+            sb.append(node.getCh());
+            node.setState(2);
+            return true;
         }
-        sb.append(node.getCh());
-        node.setState(2);
-        return true;
     }
 }
